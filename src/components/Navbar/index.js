2f181a7d2308a621ba473560ae7d49f58345b626
VinicinBraga <vinicius@bragamatta.com.br>
2022-07-29 16:01:42 -0300

    New Logo

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,15 +1,15 @@
 import React, { useContext } from "react";
 import FavoriteContext from "../../contexts/favoritesContext";
+import logo from "../Logo/logo.png";
 import "./style.css";
 
 const Navbar = () => {
   const { favoritePokemons } = useContext(FavoriteContext);
-  const logoImg =
-    "https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png";
+
   return (
     <nav>
       <div>
-        <img alt="pokeapi-logo" src={logoImg} className="navbar-img" />
+        <img alt="pokeapi-logo" src={logo} className="navbar-img" />
       </div>
       <div>{favoritePokemons.length} ❤️</div>
     </nav>
